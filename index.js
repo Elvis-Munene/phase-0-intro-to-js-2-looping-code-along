712bad4a53be2b1cdb04f2749780388ccc40db02
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-05 03:02:13 +0300

    commit tree bf0d9fb973c4afe9c515e8c9974ad3d736ef172f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      debugger;
+    }
+  
+    return gifts;
+  }
+  
+  wrapGifts(gifts);
+
+const string = ["Guadalupe","Ollie","Aki"];
+const action = 'suprise';
+
+function writeCards(string, action){
+
+  let peopleNames = [];
+  for (let i=0; i < string.length ; i++){
+    peopleNames.push(`Thank you, ${string[i]}, for the wonderful ${action} gift!`);
+  }
+  return peopleNames;
+  debugger;
+}
+console.log(writeCards(string));
+
+
+
+function countDown (x) {
+  while (x > -1) {
+    console.log(x)
+    x--;
+  }
+}
